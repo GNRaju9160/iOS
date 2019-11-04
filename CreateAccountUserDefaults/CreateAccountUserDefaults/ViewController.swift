2bcfa1b8c4e4cf38c3462e0c36527e9f014ee510
GNRaju9160 <55797337+GNRaju9160@users.noreply.github.com>
2019-11-04 10:55:35 +0530

    Add files via upload

---
+++ CreateAccountUserDefaults/CreateAccountUserDefaults/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  CreateAccountUserDefaults
+//
+//  Created by Training on 02/11/19.
+//  Copyright © 2019 Training. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class ViewController: UIViewController {
+    @IBOutlet weak var txtFldUsername: UITextField!
+    @IBOutlet weak var txtFldEmail: UITextField!
+    @IBOutlet weak var txtFldMobile: UITextField!
+    @IBOutlet weak var txtFldPass: UITextField!
+    @IBOutlet weak var txtFldConfpass: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        txtFldEmail.text?.removeAll()
+        txtFldUsername.text?.removeAll()
+        txtFldPass.text?.removeAll()
+        txtFldConfpass.text?.removeAll()
+        txtFldMobile.text?.removeAll()
+
+    }
+    @IBAction func btnCreateacc(_ sender: Any) {
+
+        UserDefaults.standard.set(txtFldMobile.text, forKey: "Mobile")
+        UserDefaults.standard.set(txtFldPass.text, forKey: "Password")
+        
+        
+        if txtFldUsername.text == "" || txtFldEmail.text == "" || txtFldMobile.text == "" || txtFldPass.text == "" || txtFldConfpass.text == "" {
+            let alert = UIAlertController(title: "Information", message: "Its Mandatort to enter all the fields", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(ok)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        } else if (txtFldPass.text != txtFldConfpass.text) {
+            let alert = UIAlertController(title: "Information", message: "Password does not match", preferredStyle: .alert
+            )
+            let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(ok)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+      
+    }
+        let nextVc = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+        self.navigationController?.pushViewController(nextVc, animated: true)
+        
+    }
+    @IBAction func btnHaveAcc(_ sender: Any) {
+        let nextVc = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+        self.navigationController?.pushViewController(nextVc, animated: true)
+        
+        
+    }
+    
+    
+}
+
