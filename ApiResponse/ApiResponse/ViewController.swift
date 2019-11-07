c3e0f98d6dc62bd5aa1508c41c96369d3607eabf
GNRaju9160 <55797337+GNRaju9160@users.noreply.github.com>
2019-11-07 12:54:04 +0530

    Add files via upload

--- ApiResponse/ApiResponse/ViewController.swift
+++ ApiResponse/ApiResponse/ViewController.swift
@@ -4,14 +4,57 @@ import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet weak var tblView: UITableView!
+    @IBOutlet weak var txtFldName: UITextField!
+    @IBOutlet weak var txtFldPsw: UITextField!
     
     let urlStr = "https://jsonplaceholder.typicode.com/posts"
     var arrResult = [Users]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         callAPI()
     }
+    // post Data API
+    @IBAction func btnSave(_ sender: Any) {
+        
+        let parameters = ["name": txtFldName.text!, "password": txtFldPsw.text!]
+        //create the url with URL
+        let url1 = URL(string: "https://jsonplaceholder.typicode.com/posts")! //change the url
+        //create the session object
+        let session = URLSession.shared
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url1)
+        request.httpMethod = "POST" //set http method as POST
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            guard error == nil else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
+    }
+
+
     func callAPI() {
         let url = URL(string: urlStr)
         Alamofire.request(url!, method: .get, parameters: nil, headers: nil).responseJSON {(response) in
