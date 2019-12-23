97cab2c8eb8916e18c9137873771f02e53c90e75
GNRaju9160 <55797337+GNRaju9160@users.noreply.github.com>
2019-12-23 12:42:14 +0530

    Add files via upload

---
+++ AlamofireSwiftyJsonMVC/Model/DataModel.swift
@@ -0,0 +1,47 @@
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import UIKit
+
+class DataModel: NSObject {
+    var id : String?
+    var index : String?
+    var guide : String?
+    var isActive : String?
+    var balance : String?
+    var picture : String?
+    var age : String?
+    var eyecolor : String?
+    var name : String?
+    var gender : String?
+    var company : String?
+    var email : String?
+    var phone : String?
+    var address : String?
+    var about : String?
+    var lattitude : String?
+     var longitude : String?
+    
+    func getData(_ dict: JSON){
+         id = dict["id"].stringValue
+         index = dict["index"].stringValue
+         guide = dict["guid"].stringValue
+         isActive = dict["isActive"].stringValue
+         balance = dict["balance"].stringValue
+         picture = dict["picture"].stringValue
+         age = dict["age"].stringValue
+         eyecolor = dict["eyeColor"].stringValue
+         name = dict["name"].stringValue
+        gender = dict["gender"].stringValue
+        company = dict["company"].stringValue
+        email = dict["email"].stringValue
+        phone = dict["phone"].stringValue
+        address = dict["address"].stringValue
+        about = dict["about"].stringValue
+        lattitude = dict["latitude"].stringValue
+        longitude = dict["longitude"].stringValue
+       
+        
+    }
+}
